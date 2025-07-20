c815149b30fc053e006903de45737555081fedc6
teodorat63 <teodoratomic300@gmail.com>
2025-07-20 15:19:11 +0200

    Konacno sredjeno

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import { Home } from './pages/home'
+import { Login } from './pages/login'
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="login" element={<Login />} />
+    </Routes>
+  )
+
+}
+
+export default App
---
+++ client/src/pages/login.jsx
@@ -0,0 +1,15 @@
+import { NavLink } from "react-router";
+
+export function Login() {
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <p>Please enter your credentials to log in.</p>
+
+            <NavLink to="/"> Link </NavLink>
+
+        </div>
+
+
+    );
+}
