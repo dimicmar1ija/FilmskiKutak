402fa7ad8b9dd960c1c530a2ecb67f4db627f2a8
teodorat63 <teodoratomic300@gmail.com>
2025-07-20 15:26:07 +0200

    Dodat TanStack - koristi se za fetch

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -6,6 +6,9 @@ export function Login() {
             <h1>Login Page</h1>
             <p>Please enter your credentials to log in.</p>
 
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+
             <NavLink to="/"> Link </NavLink>
 
         </div>
